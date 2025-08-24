9f8cb1dbfe01823ecc0d1f8214e2a68cb43ab178
Ehtesham Nawaz <nawazehtesham04@gmail.com>
2025-08-24 16:01:55 +0530

    fix clerk hydration on navbar comp

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -159,7 +159,7 @@ export default function DashboardPage() {
                     Username
                   </label>
                   <div className="flex rounded-md shadow-sm border border-gray-300 focus-within:border-blue-500 focus-within:ring-1 focus-within:ring-blue-500">
-                    <span className="w-16 inline-flex items-center px-3 bg-gray-50 border-r border-gray-300 text-gray-500 text-sm">
+                    <span className="w-60 inline-flex items-center px-3 bg-gray-50 border-r border-gray-300 text-gray-500 text-sm">
                       {baseUrl}/
                     </span>
                     <Input
